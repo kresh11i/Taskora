253b83615833c1a4cd4e8dbd8f6f9bff2e237bb5
Kresh <srikresh11@gmail.com>
2026-04-03 14:13:26 +0530

    Frontend port fix

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,6 @@
-import axios from "axios"
-import dotenv from "dotenv"
-dotenv.config();
+import axios from "axios";
+
 export const api = axios.create({
-    baseURL: process.env.VITE_API_URL ,
-    withCredentials: true,
-})
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
